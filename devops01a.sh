29e3ed2e013edac2eac6d38bc280f4fbcc508300
Ushir <ushir@yahoo.com>
2019-05-06 09:08:57 -0700

    Almost final version

--- devops01a.sh
+++ devops01a.sh
@@ -12,6 +12,7 @@ Print_Disk() {
 }
 
 # Print CPU information
+# Usage of systeminfo ; will work only on Windows system
 Print_CPU() {
     echo "----- CPU Information -----"
     cmd1=$(echo "$sysout1" | grep  "Processor")
@@ -20,6 +21,8 @@ Print_CPU() {
     echo "$cmd2"
 }
 
+# Print CPU information
+# Usage of /proc/cpuinfo; will work only on Unix system
 Print_CPU_1(){
     echo "----- Number of Cores Information -----"
     cmd1=$("nproc")
@@ -29,6 +32,29 @@ Print_CPU_1(){
     echo "$(cat /proc/cpuinfo | grep -m 1 'model')"
     echo "$(cat /proc/cpuinfo | grep -m 1 'model name')"
     echo "$(cat /proc/cpuinfo | grep -m 1 'cpu MHz')"
+}
+
+# Print CPU information
+# Looks most correct, but copied from google result; will work only on Unix
+Print_CPU_2() {
+    echo "----- CPU Information -----"
+    cmd1=$(cat /proc/cpuinfo | \
+        awk -v FS=':' '                                       \
+            /^physical id/ { if(num_cpu <$2)  { num_cpu=$2 } }     \
+            /^cpu cores/   { if(num_cores<$2){ num_cores=$2 } }   \
+            /^processor/   { if(num_units<$2){ num_units=$2 } }   \
+            /^model name/  { model=$2 }                         \
+                                                        \
+            END{                                                \
+                num_cpu=(num_cpu+1);                                 \
+                num_units=(num_units+1);                             \
+                                                      \
+                print "CPU model:",model;                          \
+                print num_cpu,"CPU,",num_cores,"physical cores per CPU, total",num_units,"logical CPU units" \
+            }')
+
+    echo "$cmd1"
+
 }
 
 # Print RAM information
@@ -56,8 +82,9 @@ Print_Mac_IP() {
 echo "----- Main -----"
 sysout1=$(systeminfo)
 Print_Disk
-Print_CPU
-Print_CPU_1
+#Print_CPU
+#Print_CPU_1
+Print_CPU_2
 Print_Ram
 Print_Mac_IP
 
